fe9860355d438bd0f14b4310eeb2edbe9bca43db
Allen <allenvs@mail.ru>
2020-02-23 16:08:12 +0300

    added task setup code

---
+++ app.js
@@ -0,0 +1,11 @@
+function QuestionsMarks(str) { 
+
+    // code goes here  
+    return str; 
+  
+};
+
+var str = "arrb6???4xxbl5???eee5";
+
+// keep this function call here 
+console.log(QuestionsMarks(readline()));
