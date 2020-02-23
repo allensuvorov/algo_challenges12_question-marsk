7b18c803593c44c252e4b5432201ac00ceabf49c
Allen <allenvs@mail.ru>
2020-02-24 01:09:33 +0300

    adde code that finds numbers and counts question marks between them

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 function QuestionsMarks(str) { 
+    var char;
 
-    // code goes here  
-    return str; 
-  
+    // go along the string
+    for (let i=0;i<str.length;i++) {
+        // get character number i
+        char = str.charAt(i); 
+        
+        // check if char is a number
+        if (!isNaN(char)) {
+            var qmarks = 0;
+            
+            for (var n=i+1;n<str.length;n++) {
+                if (str.charAt(n)=="?") qmarks++;
+                if (!isNaN(str.charAt(n))) break;
+            };
+            
+            console.log(char, i, qmarks, n);
+            i = n-1;
+        };
+    };
+        // code goes here 
+        return str;
 };
-
 var str = "arrb6???4xxbl5???eee5";
 
 // keep this function call here 
